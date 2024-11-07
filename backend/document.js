cf0330edcd232c38136f1b8bb968f9ab7773b8a7
tminhnhat <nhatco@gmail.com>
2024-11-07 22:55:52 +0700

    aa

--- backend/document.js
+++ backend/document.js
@@ -16,15 +16,16 @@ const generateDocument = (templatePath, data) => {
 
 const handleDocumentGeneration = async (req, res) => {
   try {
-    const data = req.body;
+    const datachung = req.body;
+    const data = datachung.data;
     if (!data) {
       return res.status(400).send('No data provided.');
     }
 
-    const templatePath = path.join(__dirname, 'maubieu', `${data.tenmaubieu}.docx`);
-    const outputFilePath = path.join(__dirname, 'ketqua', `${data.tenfileduoctao}.docx`);
+    const templatePath = path.join(__dirname, 'maubieu', `${datachung.tenmaubieu}.docx`);
+    const outputFilePath = path.join(__dirname, 'ketqua', `${datachung.tenfileduoctao}.docx`);
 
-    const result = await generateDocument(templatePath, data.data);
+    const result = await generateDocument(templatePath, datachung.data);
     saveDocument(outputFilePath, result);
 
     res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
